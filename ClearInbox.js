02cd556ccb12a2bbe048a9888c8eb87319bed264
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-14 08:19:20 +0100

    reading trix and case

--- ClearInbox.js
+++ ClearInbox.js
@@ -1,20 +1,50 @@
-function myFunction() {
+function LoadConfiguration() {
+  var configuration = SpreadsheetApp.openById("1-qb1wmRiDWJTq5n5T3ItkWJmHjU-BhGYFe9e439MFtc");
+  var SheetPlaces = configuration.getSheetByName("Vacations");
 
-  var subjectsToArchive = ['80% of budget reached']
-  var subjectsStartToArchive=['GCP CES Daily Report']
+  var data = SheetPlaces.getDataRange().getValues();
+  data.shift();// Remove header 
+  var items = [];
+
+  data.forEach(function (row) {
+    var element={ 'Mask': row[0], 'Operator': row[1], 'Action': row[2] }
+    items.push(element);
+  });
+  return items;
+}
+
+function DoEqualsWork(subject, configurationItem) {
+  if (subject == configurationItem.Mask) {
+    Logger.Log("Wooho")
+  }
+}
+
+function DoActionOnThread(subject, configuration) {
+  for (var i = 0; i < configuration.length; i++) {
+    switch (configuration[i].Operator) {
+      case 'Equals':
+        DoEqualsWork(subject, configuration[i]);
+        break;
+    }
+  }
+}
+
+function myFunction() {
+  var configuration = LoadConfiguration();
 
   var threads = GmailApp.getInboxThreads();
   for (var i = 0; i < threads.length; i++) {
-
-    var subject = threads[i].getFirstMessageSubject()
-    Logger.log(subject);
-    if (subjectsToArchive.includes(subject)) {
-      //threads[i].moveToArchive();
-    }
-    for(var j=0;j<subjectsStartToArchive.length;j++){
-      if (subject.startsWith(subjectsStartToArchive[j])){
-        threads[i].moveToArchive();
-      }
-    }
+    DoActionOnThread(threads[i].getFirstMessageSubject(),configuration)
+    // var subject = threads[i].getFirstMessageSubject()
+    // Logger.log(subject);
+    // if (subjectsToArchive.includes(subject)) {
+    //   //threads[i].moveToArchive();
+    // }
+    // for (var j = 0; j < subjectsStartToArchive.length; j++) {
+    //   if (subject.startsWith(subjectsStartToArchive[j])) {
+    //     threads[i].moveToArchive();
+    //   }
+    // }
   }
+
 }
